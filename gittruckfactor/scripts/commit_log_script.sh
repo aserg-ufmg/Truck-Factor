b39d5d70a87822c890ebc751ed2e681a8b4b790c
Guilherme Avelino <gavelino@gmail.com>
2018-04-11 11:39:06 -0300

    Improve commit log process: change separation character and add stripAccents function

--- gittruckfactor/scripts/commit_log_script.sh
+++ gittruckfactor/scripts/commit_log_script.sh
@@ -11,7 +11,7 @@ cd $path
 git config diff.renameLimit 999999 
 
 #Extract commit information
-git log --pretty=format:"%H;%an;%ae;%at;%cn;%ce;%ct;%f"  > commitinfo.log
+git log --pretty=format:"%H-;-%aN-;-%aE-;-%at-;-%cN-;-%cE-;-%ct-;-%f"  > commitinfo.log
 
 #Extract and format commit files information
 git log --name-status --pretty=format:"commit	%H" --find-renames > log.log
--- gittruckfactor/src/aserg/gtf/task/extractor/GitLogExtractor.java
+++ gittruckfactor/src/aserg/gtf/task/extractor/GitLogExtractor.java
@@ -1,14 +1,17 @@
 package aserg.gtf.task.extractor;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import aserg.gtf.dao.LogCommitDAO;
@@ -32,24 +35,35 @@ public class GitLogExtractor extends AbstractTask<Map<String, LogCommitInfo>>{
 		int countcfs = 0;
 		try{	
 			LOGGER.info("Extracting logCommits...  "+repositoryPath);
-			BufferedReader br = new BufferedReader(new FileReader(
-					repositoryPath + fileName));
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(repositoryPath + fileName), "UTF8"));
 			String sCurrentLine;
 			String[] values;
 			while ((sCurrentLine = br.readLine()) != null) {
-				values = sCurrentLine.split(";");
-				if (values.length<7)
-					LOGGER.error("Problem in line  " + countcfs + ". Too much columns.");
+				sCurrentLine = StringUtils.stripAccents(sCurrentLine);
+				values = sCurrentLine.split("-;-");
+				values = removeSemicolon(values, "&");
+				if (values.length<7 || values.length>8)
+					System.err.println("Problem in line  " + countcfs + ". Wrong number of columns.");
 				Date authorDate = !values[3].isEmpty() ? new Timestamp(Long.parseLong(values[3]) * 1000L) : null;
 				Date commiterDate = !values[6].isEmpty() ? new Timestamp(Long.parseLong(values[6]) * 1000L) : null;
+				if (authorDate == null && commiterDate == null){
+					System.err.println("Commit without date. Ignoring commit: "+values[0]);
+					continue;
+				}
 				String msg = (values.length == 8) ? values[7] : "";
-
+				
+				countcfs++;
+				
+				if ((values[1]+values[2]+values[4]+values[5]).isEmpty()){
+					System.err.println("Ignoring commit without developer info in  " + values[0]);
+					continue;
+				}
 				mapCommits.put(values[0],
 						new LogCommitInfo(repositoryName,
 								values[0], values[1], values[2],
 								authorDate, values[4], values[5],
 								commiterDate, msg));
-				countcfs++;
+//				System.out.println(countcfs);
 			}
 			insertFiles(repositoryName, mapCommits);
 			br.close();
@@ -59,11 +73,21 @@ public class GitLogExtractor extends AbstractTask<Map<String, LogCommitInfo>>{
 			throw new Exception("File not found: " + repositoryPath + fileName, e);
 		}
 		catch(Exception e ){
-			throw new Exception(String.format("Error in file %s, line %d%", repositoryName, countcfs));
+			throw new Exception(String.format("Error in file %s, line %d", repositoryName, countcfs));
 		}
 				
 		return mapCommits;
 		
+	}
+
+	// Remove internal semicolon character. Useful to further data process 
+	private String[] removeSemicolon(String[] values, String newStr) {
+		String[] newValues = new String[values.length];
+		int i = 0;
+		for (String value : values) {
+			newValues[i++] = value.replace(";", newStr);
+		}
+		return newValues;
 	}
 
 
@@ -81,21 +105,27 @@ public class GitLogExtractor extends AbstractTask<Map<String, LogCommitInfo>>{
 		
 	}
 	
-	
 	private void insertFiles(String projectName, Map<String, LogCommitInfo> mapCommit) throws IOException{
 		LOGGER.info(projectName+": Extracting logCommitFiles...");
-		BufferedReader br = new BufferedReader(new FileReader(repositoryPath+"commitfileinfo.log"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(repositoryPath+"commitfileinfo.log"), "UTF8"));
 		String sCurrentLine;
 		String[] values;
 
 		while ((sCurrentLine = br.readLine()) != null) {
 			values = sCurrentLine.split(";");
 			String sha = values[0];
-			LogCommitInfo commit = mapCommit.get(sha);
-			commit.addCommitFile(new LogCommitFileInfo(commit, values[1], values[2], values[3]));
+			if (mapCommit.containsKey(sha)){
+				LogCommitInfo commit = mapCommit.get(sha);
+				commit.addCommitFile(new LogCommitFileInfo(commit, values[1], values[2], values[3]));
+			}
+			else
+				System.err.println("Ignoring file " + values[3] + " Sha: "+sha);
 		}
 		br.close();
 	}
+
+
+	
 
 
 
